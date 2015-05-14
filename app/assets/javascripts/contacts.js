9982c22aa30120e2e8cc88dfc86ff53aa56463f0
Dave Liggat <dliggat@gmail.com>
2015-05-13 17:10:01 -0700

    Add clickable row for show action

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,2 +1,9 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+(function() {
+
+  $(document).ready(function() {
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });
+  });
+
+})();
