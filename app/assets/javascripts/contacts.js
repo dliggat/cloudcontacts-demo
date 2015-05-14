fb77d75a65a918f4cdc028c7f28667c71d0be766
Dave Liggat <dliggat@gmail.com>
2015-05-14 00:50:45 -0700

    Adding an application helper for adding fields

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,16 +1,18 @@
-(function() {
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g");
+  $(link).first().before(content.replace(regexp, new_id));
+}
 
-  $(document).ready(function() {
-    $(".clickable-row").click(function() {
-        window.document.location = $(this).data("href");
-    });
 
-    $(".remove_link").click(function(event) {
-      window.blar = $(event.target);
-      $(event.target).prevAll("input[type=hidden]").first().val("1");
-      $(event.target).parents('.expandable_fields').first().hide();
-    });
+$(document).ready(function() {
+  $(".clickable-row").click(function() {
+      window.document.location = $(this).data("href");
   });
 
+  $(".remove_link").click(function(event) {
+    $(event.target).prevAll("input[type=hidden]").first().val("1");
+    $(event.target).parents('.expandable_fields').first().hide();
+  });
 
-})();
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def link_to_add_fields(name, f, association, cssClass, title)
+    # Particularly helpful: http://stackoverflow.com/questions/23777751/link-to-add-fields-unobtrusive-javascript-rails-4
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to name, "javascript:void(0)", onclick: "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", class: cssClass, title: title
+  end
 end
